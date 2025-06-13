47d2bbd6013a8a4b1d1c2da6d5e9a650a0debeeb
phantosmax <phantosmax@gmail.com>
2025-06-13 23:35:52 +0800

    Delete build directory

--- build/index.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { config } from 'dotenv';
-// Load environment variables from .env file
-config();
-// Import and start the server after environment is loaded
-import('./server.js');
-//# sourceMappingURL=index.js.map
